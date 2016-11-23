1c3516d6db7459c1f3d6280b5a91aa063c65dc37
wangxiang <wangxiangc@gmail.com>
2016-11-23 14:04:25 +0800

    java annotation intdef

---
+++ Javascript/node.js/example/readout.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/readtimeout', function(req, res) {
+	console.log('get request, not response');
+  //res.send('hello world');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
